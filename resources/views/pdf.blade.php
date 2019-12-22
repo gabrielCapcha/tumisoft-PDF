fde88e4dfe67eee1c4ba55156e641c735f2efd43
GABRIEL <nelson.capcha@unas.edu.pe>
2019-12-22 14:14:45 -0500

    tumisoft-PDF

---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Curriculum</title>
+		<style>
+		    #principal{
+		        border: solid 2px;
+		        padding: 30px;
+		        width: 80%;
+		        margin: 0 auto;
+		        background: #bce8d1;
+		        border-radius: 20px;
+		    }
+		    
+		    legend{
+                font-family: courier,arial,helvética;
+		        border: solid 1px #0000c5;
+		        padding: 5px;
+		        font-weight: bold;
+		        border-radius: 20px;
+		        background: #a6daea;
+		    }
+		    
+		    body{
+		        background: #a6daea;
+		    }
+		    
+		</style>
+	</head>
+	<body>
+	    <div id="principal">
+	       <img src=""/>
+	       <h2>Nelson Gabriel Capcha Anaya</h2> 
+	       <p>Calle Atuncoya 169 Tahuantinsuyo - Independencia</p>
+	       <p>gabrielanaya740@gmail.com, Tel. 949223685</p>
+	       
+	       <fieldset>
+	           <legend>Datos generales</legend>
+	                <ul>
+	                    <li>Fecha de Nacimiento: 07-04-2000</li>
+	                    <li>DNI: 77499091</li>
+	                    <li>Nacionalidad: Peruana</li>
+	                    <li>Estado civil: Soltero</li>
+	                </ul>
+	       </fieldset>
+	       <br/>
+	       <fieldset>
+	            <legend>Información académica</legend>
+	            <ul>
+	                <li>Estudios Primarios: Colegio Parroquial San Pablo(Argentina)</li>
+	                <li>Estudios Secundarios: P.C.N.B.R de Nuestra Señora de Guadalupe</li>
+	                <li>Estudios Universitario: Ingeniería en informática y sistemas, Universidad Nacional Agraria de la Selva(Actual)</li>
+	            </ul>
+	       </fieldset>
+	       <br/>
+	       <fieldset>
+	           <legend>Experiencia Laboral</legend>
+	                <ul>
+	                    <li>Ninguna</li>
+	                </ul>
+	       </fieldset>
+	       <br/>
+	       <fieldset>
+	           <legend>Películas y libros</legend>
+	                <ul>
+	                    <li>Película: El señor de los anillos</li>
+	                    <li>Película: Lucy</li>
+	                    <li>Película: Harry Potter</li>
+	                    <li>Libro: El retrato de Dorian Grey</li>
+	                    <li>Libro: It(Eso)</li>
+	                </ul>
+	       </fieldset>
+	    </div>
+	</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tumisoft</title>
+
+        <style>
+            #content{
+                border: solid 0px;
+		        padding: 30px;
+		        width: 80%;
+                margin: 0 auto;
+		        background: #D9E709;
+		        border-radius: 20px;
+            }
+        </style>
+    </head>
+    <body>
+            <div id="content">
+                <p style = "font-family:courier,arial,helvética;">Descargar Archivo</p>
+                    <ul>
+                    <a href="{{ route('imprimir') }}">Imprimir Tumisoft-pdf</a>  
+                    </ul>
+            </div>
+    </body>      
+</html>
